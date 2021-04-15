d5b985030a9b203cd3018bcafd45e72d74bdd967
Alex <a.henkel@mgga.de>
2021-04-15 11:00:21 +0200

    updated evaluation dimension

--- scripts/sensor_processor/evaluation_decoder.py
+++ scripts/sensor_processor/evaluation_decoder.py
@@ -14,7 +14,7 @@ class EvaluationDecoder:
 
     @classmethod
     def read_folder(cls, folder_name: str) -> np.ndarray:
-        predictions = read_csvs_in_folder(folder_name, 'evaluation', 4)
+        predictions = read_csvs_in_folder(folder_name, 'evaluation', 5)
 
         return predictions
 
