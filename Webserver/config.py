b776bc910f4cd553d2b06e023ab68647150d9d33
Alex <a.henkel@mgga.de>
2021-06-11 13:50:33 +0200

    allow POST json

--- Webserver/config.py
+++ Webserver/config.py
@@ -6,7 +6,7 @@ import os
 version = 1.3
 secret_key_iterations = 42
 
-ALLOWED_EXTENSIONS = {'zip', 'mkv', 'csv', '3gp', 'tflite'}
+ALLOWED_EXTENSIONS = {'zip', 'mkv', 'csv', '3gp', 'tflite', 'json'}
 UPLOAD_FOLDER = 'uploads'
 
 RECORDINGS_FOLDER = os.path.join(UPLOAD_FOLDER, 'recordings')
